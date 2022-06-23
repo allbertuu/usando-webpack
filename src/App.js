9933135038d9990bbf11be4d8ce168c0c284cc8d
Alberto <albertovinicius3@gmail.com>
2022-06-23 18:15:43 -0300

    last commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Configurando e usando Webpack do zero! 🚀</h1>
+      <p>Toda essa página foi resultado de uma compilação 😊</p>
+    </div>
+  );
+}
